6933a827cd63fb5a7d68dc5edddee70a7133e980
Madysan Miller <madysanmiller2020@gmail.com>
2023-10-06 11:15:40 -0400

    completed tasks

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+const getResources = async () => {
+  return db("resources");
+};
+const getResourceById = async (resource_id) => {
+  return db("resources").where("resource_id", resource_id).first();
+};
+const insertResource = async (resource) => {
+  const [id] = await db("resources").insert(resource);
+  return getResourceById(id);
+};
+module.exports = {
+  getResources,
+  insertResource,
+};
